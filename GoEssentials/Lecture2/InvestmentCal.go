a2952c687112e9253280a804fd79f39026029bb2
Sarthak <sarthak2019fractal@gmail.com>
2024-09-06 16:56:26 +0530

    Lecure2

--- GoEssentials/Lecture2/InvestmentCal.go
+++ GoEssentials/Lecture2/InvestmentCal.go
@@ -5,12 +5,56 @@ import (
 	"math"
 )
 
+// func main() {
+// 	var investmentAmount = 1000
+// 	var expectedReturnRate = 5.5
+// 	var years = 10
+
+// 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+// 	fmt.Println(futureValue)
+// }
+
+// func main() {
+// 	var investmentAmount float64= 1000
+// 	var expectedReturnRate float64= 5.5
+// 	var years float64= 10
+
+// 	var futureValue = investmentAmount* math.Pow(1+expectedReturnRate/100, years)
+// 	fmt.Println(futureValue)
+// }
+
+// func main() {
+// 	var investmentAmount,years float64= 1000,10
+//     expectedReturnRate := 5.5
+
+// 	futureValue := investmentAmount* math.Pow(1+expectedReturnRate/100, years)
+// 	fmt.Println(futureValue)
+// }
+
+
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	const inflationRate=2.5
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
+	
+
+
+	fmt.Print("InvestmentAmount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("ExpectedReturnRate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+
+	var futureValue = investmentAmount* math.Pow(1+expectedReturnRate/100, years)
+
+	futureRealValue:=futureValue/math.Pow((1+inflationRate/100),years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
+
+
